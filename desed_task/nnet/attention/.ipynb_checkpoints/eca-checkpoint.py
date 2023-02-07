d378ff53d88a9adcf1f627159b1535555ca1c1c9
Tanmay Khandelwal <36072284+rythmm24@users.noreply.github.com>
2023-02-07 09:48:24 +0530

    low comp setup

---
+++ desed_task/nnet/attention/.ipynb_checkpoints/eca-checkpoint.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+import math
+class ConvBNReLU(nn.Sequential):
+    def __init__(self, in_planes, out_planes, kernel_size=3, stride=1, groups=1):
+        padding = (kernel_size - 1) // 2
+        super(ConvBNReLU, self).__init__(
+            nn.Conv2d(in_planes, out_planes, kernel_size, stride, padding, groups=groups, bias=False),
+            nn.BatchNorm2d(out_planes),
+            nn.ReLU6(inplace=True)
+        )
+
+class eca_layer(nn.Module):
+    """Constructs a ECA module.
+    Args:
+        channel: Number of channels of the input feature map
+        k_size: Adaptive selection of kernel size
+    """
+    def __init__(self, channel, gamma=2, beta=1):
+        super(eca_layer, self).__init__()
+
+        if channel is not None:
+            t = int(abs(math.log(channel, 2) + beta) / gamma)
+            kernel_size = max(t if t % 2 else t + 1, 3)
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.conv = nn.Conv1d(1, 1, kernel_size=kernel_size, padding=(kernel_size - 1) // 2, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        # x: input features with shape [b, c, h, w]
+        b, c, h, w = x.size()
+
+        # feature descriptor on the global spatial information
+        y = self.avg_pool(x)
+
+        # Two different branches of ECA module
+        y = self.conv(y.squeeze(-1).transpose(-1, -2)).transpose(-1, -2).unsqueeze(-1)
+
+        # Multi-scale information fusion
+        y = self.sigmoid(y)
+
+        return x * y.expand_as(x)
